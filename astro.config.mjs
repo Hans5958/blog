36215d973d06c80f298a54faa12d75d9c0f0f271
Hans5958 <hans5958@outlook.com>
2023-01-25 22:10:57 +0700

    Add @astrojs/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,19 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import compress from "astro-compress";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hans5958.github.io/blog',
   base: '/blog',
   integrations: [mdx(), sitemap(), tailwind(), vue(), 
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp'
+    }), 
     import.meta.env.MODE === "production" && compress({
-      img: false,
-    })
-  ],
+    img: false
+  })],
   markdown: {
     shikiConfig: {
       theme: 'github-light',
