a72f1434537bc1ee3c7ef19d08c93501e8d6eb5f
Hans5958 <hans5958@outlook.com>
2023-07-02 23:43:52 +0700

    Add meta cover images if missing
    Resolves #31

---
+++ src/pages/assets/og/[...route].ts
@@ -0,0 +1,51 @@
+import { getCollection } from 'astro:content'
+import { remark } from 'remark';
+import remarkStrip from 'strip-markdown'
+import { OGImageRoute } from 'astro-og-canvas'
+
+const posts = {}
+
+for (const post of await getCollection('posts')) {
+	if (post.data.cover) continue
+	posts[post.slug] = {}
+	posts[post.slug].title = post.data.title
+	let excerpt = ""
+
+	if (post.data.excerpt) {
+		const excerptUnified = await remark()
+			.use(remarkStrip)
+			.process(post.data.excerpt)
+		excerpt = excerptUnified.toString()
+	}
+		posts[post.slug].description = excerpt 
+}
+
+
+export const { getStaticPaths, get } = OGImageRoute({
+	param: 'route',
+
+	pages: posts,
+
+	getImageOptions: (path, post) => ({
+		title: post.title,
+		description: post.description,
+		bgGradient: [[51, 65, 85], [100, 116, 139]],
+		logo: {
+			path: './src/assets/images/site-logo.png',
+			size: [277 / 1.5, 128 / 1.5]
+		},
+		font: {
+			title: {
+				lineHeight: 1.2,
+				families: ['Lato']
+			},
+			description: {
+				size: 30
+			}
+		},
+		padding: 60,
+		fonts: [
+			'https://api.fontsource.org/v1/fonts/lato/latin-400-normal.ttf'
+		]
+	}),
+})
