3b4086a1641193a536ef2b46902caa135a873512
Hans5958 <hans5958@outlook.com>
2023-07-28 21:01:48 +0700

    Multiple changes (28/07/2023)
    Use the correct version of @astrojs/mdx
    Use tailwind-nested instead of sass
    Use Lightning CSS
    Fix dev-only errors on TOC styles
    Fix version to 3.0.0 (generation 3)
    Change spaces to tabs in some files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,41 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-import vue from "@astrojs/vue";
-import compress from "astro-compress";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from "@astrojs/tailwind"
+import vue from "@astrojs/vue"
+import compress from "astro-compress"
+import lightningcss from 'vite-plugin-lightningcss'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hans5958.github.io/blog',
-  base: '/blog',
-  integrations: [mdx(), sitemap(), tailwind(), vue(), 
-    import.meta.env.MODE === "production" && compress({
-      img: false
-    })
-  ],
-  markdown: {
-    shikiConfig: {
-      theme: 'github-light',
-      langs: []
-    }
-  },
-  experimental: {
-    assets: true
-  },
-  image: {
-    service: {
-      entrypoint: 'astro/assets/services/sharp' 
-    },
-  },
-});
+	site: 'https://hans5958.github.io/blog',
+	base: '/blog',
+	integrations: [mdx(), sitemap(), tailwind({
+		applyBaseStyles: false
+	}), vue(),
+	import.meta.env.MODE === "production" && compress({
+		img: false
+	})
+	],
+	markdown: {
+		shikiConfig: {
+			theme: 'github-light',
+			langs: []
+		}
+	},
+	experimental: {
+		assets: true
+	},
+	image: {
+		service: {
+			entrypoint: 'astro/assets/services/sharp'
+		},
+	},
+	vite: {
+		plugins: [
+			lightningcss({
+				browserslist: '>= 0.25%',
+			}),
+		]
+	}
+})
