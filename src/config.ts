6bf8dc268854a20ea6d4f5fd18f07467de5bb8e5
Hans5958 <hans5958@outlook.com>
2024-12-21 11:46:32 +0700

    Remove trailing semicolor, adjust imports

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
-import childProcess from "node:child_process";
+import childProcess from "node:child_process"
 
-const today = new Date();
+const today = new Date()
 
-export const SITE_TITLE = 'Blog of Hans';
+export const SITE_TITLE = 'Blog of Hans'
 export const SITE_DESCRIPTION = 'The blog of Hans5958.'
 export const SITE_AUTHOR = 'Hans5958'
 export const SITE_REPOSITORY = 'https://github.com/Hans5958/blog'
@@ -10,13 +10,13 @@ export const SITE_TWITTER = 'Hans5958'
 export const COPYRIGHT_YEAR = today.getFullYear()
 export const BASE_URL = import.meta.env.BASE_URL.replace(/\/$/, '')
 
-export const COMMIT_HASH = childProcess.execSync('git rev-parse HEAD')?.toString().trim();
-export const COMMIT_HASH_SHORT = childProcess.execSync('git rev-parse --short HEAD')?.toString().trim();
+export const COMMIT_HASH = childProcess.execSync('git rev-parse HEAD')?.toString().trim()
+export const COMMIT_HASH_SHORT = childProcess.execSync('git rev-parse --short HEAD')?.toString().trim()
 
 export interface BaseProps {
-	title?: string;
-	description?: string;
-	coverSrc?: string;
-	coverAlt?: string;
-	noAnalytics?: boolean;
+	title?: string
+	description?: string
+	coverSrc?: string
+	coverAlt?: string
+	noAnalytics?: boolean
 }
