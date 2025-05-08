44bae606f736420197b2df78750116ad8b3c8a40
Hans5958 <hans5958@outlook.com>
2025-05-09 00:37:51 +0700

    Fix Twitter meta tag

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const SITE_TITLE = 'Blog of Hans'
 export const SITE_DESCRIPTION = 'The blog of Hans5958.'
 export const SITE_AUTHOR = 'Hans5958'
 export const SITE_REPOSITORY = 'https://github.com/Hans5958/blog'
-export const SITE_TWITTER = 'Hans5958'
+export const SITE_TWITTER = '@Hans5958'
 export const SITE_MASTODON = '@Hans5958@mastodon.social'
 
 export const COPYRIGHT_YEAR = today.getFullYear()
